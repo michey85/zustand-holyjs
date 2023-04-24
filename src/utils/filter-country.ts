6d1173afe30f774fde3ecdd80483d5ad8f8771d2
Mikhail Nepomniashchii <mikhail_nepomniashchii@epam.com>
2023-04-24 20:04:48 +0200

    add a helper filterCountries

--- src/utils/filter-country.ts
+++ src/utils/filter-country.ts
@@ -3,3 +3,6 @@ import { Country, Region } from '../types';
 export const filterCountry = (country: Country, search: string, region: Region | null): boolean => {
     return country.name.toLowerCase().includes(search.toLowerCase()) && country.region.includes(region || '');
 };
+
+export const filterCountries = (countries: Country[], search: string, region: Region | null) =>
+    countries.filter((country) => filterCountry(country, search, region));
